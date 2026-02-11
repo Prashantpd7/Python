7c916b5ebe16de354134a7366cb430a924e162ab
Prashant <prashantdwivedipd7@gmail.com>
2026-02-11 15:13:33 +0530

    new concept

--- First/main.py
+++ First/main.py
@@ -1,2 +1,9 @@
-print("Hello, World!")
-print("Welcome to Python programming.")
+# Type Casting
+a = 10
+a = str(a)
+print(type(a))
+print (a)
+
+# Input from user
+name = input("Enter your name: ")
+print("Hello " + name)
