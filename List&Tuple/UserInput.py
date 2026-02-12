594adf0e41fad8e32901ab58bc48b2a9dd2f12a0
Prashant <prashantdwivedipd7@gmail.com>
2026-02-12 18:34:40 +0530

    new concept

---
+++ List&Tuple/UserInput.py
@@ -0,0 +1,6 @@
+""" WAP to ask user to enter there 3 favourite movies & store them in a list."""
+name1 = input("Enter 1st movie: ")
+name2 = input("Enter 2nd movie: ")
+name3 = input("Enter 3rd movie: ")
+movies = [name1, name2, name3]
+print(movies)
