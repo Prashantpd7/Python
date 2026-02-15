e23f6b53e4fff37752ad615b39c8d1c2dada1546
Prashant <prashantdwivedipd7@gmail.com>
2026-02-15 22:13:21 +0530

    new question

---
+++ Dictionary&Set/StoreMarks.py
@@ -0,0 +1,8 @@
+table = {}
+S1 = int(input("Enter your Maths Number: "))
+S2 = int(input("Enter your Physics Number: "))
+S3 = int(input("Enter your Chemistry Number: "))
+table.update({"Maths" : S1})
+table.update({"Physics" : S2})
+table.update({"Chemistry" : S3})
+print(table)
