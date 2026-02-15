e8c70629abf8c8865f851d8ed1db839699df081a
Prashant <prashantdwivedipd7@gmail.com>
2026-02-16 01:13:31 +0530

    new quesiton

--- Loops/SearchNumber.py
+++ Loops/SearchNumber.py
@@ -1,5 +1,7 @@
 list = (1,4,9,6,8,34,76,98,35,78,56)
 x = int(input("Enter number: "))
-n = list/x
-while n==1:
-    print(n)
+n = 0
+while n<len(list):
+    if (x==list[n]):
+        print(n)
+    n+=1
