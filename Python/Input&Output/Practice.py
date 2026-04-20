45bee505930c3803f3c04f232b704fcea5af5c66
Prashant <prashantdwivedipd7@gmail.com>
2026-04-20 21:29:34 +0530

    new changes

--- Python/Input&Output/Practice.py
+++ Python/Input&Output/Practice.py
@@ -1,8 +1,13 @@
-with open("Chatgpt.txt","r") as f:
-    data = f.read()
+# with open("Chatgpt.txt","r") as f:
+#     data = f.read()
 
-new_data = data.replace("Hello","Hii")
-print(new_data)
+# new_data = data.replace("Hello","Hii")
+# print(new_data)
 
-with open("Chatgpt.txt",'w') as f:
-    f.write(new_data)
+# with open("Chatgpt.txt",'w') as f:
+#     f.write(new_data)
+
+a = 5
+b = 7.9
+c = a + b
+print(int(c))
