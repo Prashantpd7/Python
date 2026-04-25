e4ec329854bc814ef8c4c7e54846cad00b1e4af0
Prashant <prashantdwivedipd7@gmail.com>
2026-04-25 12:03:36 +0530

    started solving practice set

---
+++ Python/Loops/List_Comprehension.py
@@ -0,0 +1,17 @@
+# nums = []
+# for i in range(1,5):
+#     nums.append(i)
+# print(nums)
+# nums.insert(3,"x")
+# nums.remove(4)
+# nums.pop(0)
+# print(nums.count(3))
+# print(nums)
+# nums1 = nums.copy()
+# print(nums1)
+
+
+t = (1,2,2,3)
+print(t.count(2))
+print(t.index(3))
+print(t)
